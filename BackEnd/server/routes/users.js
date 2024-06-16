30acb38c37bb1d724a43797dd6f8a5eec3ffaa62
Samitha <samitha.nillupul1440@gmail.com>
2024-06-16 20:15:26 +0530

    user controller

--- BackEnd/server/routes/users.js
+++ BackEnd/server/routes/users.js
@@ -7,3 +7,8 @@ const router = express.Router();
 //READ
 router.get("/:id",verifyToken,getUser);
 router.get(":/id/friends",verifyToken,getUserFriends);
+
+//UPDATE 
+router.patch(":id/:friendId",verifyToken,addRemoveFriend);
+
+export default router;
