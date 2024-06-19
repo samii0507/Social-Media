dfeb2ff9450c644f3800fbdecb6bd33b7ec5388e
Samitha <samitha.nillupul1440@gmail.com>
2024-06-19 17:04:35 +0530

    loogin

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,35 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+
 const LoginPage = () => {
-    return <div>index</div>;
-  };
-  
-  export default LoginPage;
-  
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontSize="32px" fontWeight="bold" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{mb:"1.5rem"}}>
+          Welcome to socipedia
+        </Typography>
+        
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
