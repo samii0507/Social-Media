a4713e3e6deb1c871d0a890943f540674ba345e3
Samitha <samitha.nillupul1440@gmail.com>
2024-06-20 22:51:07 +0530

    index homepage

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,34 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import NavBar from "scenes/navBar";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
-  return <Box>
-    <NavBar/>
-  </Box>;
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
+  return (
+    <Box>
+      <NavBar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default HomePage;
